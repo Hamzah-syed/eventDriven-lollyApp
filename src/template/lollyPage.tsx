879fa29244051d0b82919a1acee0085c9eca0f7e
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-11-04 18:16:22 +0500

    test

--- src/template/lollyPage.tsx
+++ src/template/lollyPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+import { Box } from "../utils/box";
+import Header from "../components/header";
+import Lolly from "../components/lolly";
 
 export const query = graphql`
   query MyQuery($slug: String!) {
@@ -11,16 +14,50 @@ export const query = graphql`
         flavourTop
         flavourMiddle
         flavourBottom
+        slug
       }
     }
   }
 `;
 
-const LollyPage = ({ data }) => {
-  console.log(data);
+const LollyPage = ({
+  data: {
+    Lollies: { GetLollyBySlug },
+  },
+}) => {
+  console.log(GetLollyBySlug);
   return (
     <div>
-      <h1>hamzah</h1>
+      <Header />
+      <Box p="12px">
+        <div className="freezedLollyCardWrapper">
+          <Lolly
+            flavourTop={GetLollyBySlug.flavourTop}
+            flavourBottom={GetLollyBySlug.flavourBottom}
+            flavourMiddle={GetLollyBySlug.flavourMiddle}
+          />
+
+          <div className="freezedLollyData">
+            <div className="linkWrapper">
+              <h4>Share this link with your frined</h4>
+              <p>{`${location.origin}/lollies/${GetLollyBySlug.slug}`}</p>
+            </div>
+            <Box p="20px" className="freezedLollyCard">
+              <h1>to: {GetLollyBySlug.to}</h1>
+              <p>{GetLollyBySlug.message}</p>
+              <h3>From: {GetLollyBySlug.from}</h3>
+            </Box>
+            <div className="recivermessage">
+              <p>
+                {GetLollyBySlug.from} made this virtual lollipop for you. You
+                can <Link to="/createLolly"> make your own</Link> to send to a
+                friend who deserve some sugary treat which won't rot their
+                teeth...
+              </p>
+            </div>
+          </div>
+        </div>
+      </Box>
     </div>
   );
 };
